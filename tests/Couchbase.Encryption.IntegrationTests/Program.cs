43f4a44344dd1b0b75ccca9848054bf81eeac72c
Jeff Morris <jeffry.morris@couchbase.com>
2020-12-17 19:26:15 -0800

    Add VS solution and projects

---
+++ tests/Couchbase.Encryption.IntegrationTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Couchbase.Encryption.IntegrationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/Couchbase.Encryption.UnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Couchbase.Encryption.UnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
